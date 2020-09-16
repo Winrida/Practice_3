16dc85fd0a7f8e8128bade16db83494d245b4d5a
Winrida <winrida@yandex.ru>
2020-09-16 10:08:21 +0300

    Initial commit

---
+++ src/ru/mirea/pr3/Square.java
@@ -0,0 +1,31 @@
+package ru.mirea.pr3;
+
+public class Square extends Rectangle {
+
+    public Square() {
+        super();
+    }
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    public Square(double side, String color, boolean filled) {
+        super(side, side, color, filled);
+    }
+
+    public double getSide() {
+        return super.getWidth();
+    }
+
+    public void setSide(double side) {
+        super.setLength(side);
+        super.setWidth(side);
+    }
+
+    @Override
+    public String toString() {
+        return "Shape: square, side: " + this.width + ", color: " + color +
+                ", is filled: " + filled;
+    }
+}
